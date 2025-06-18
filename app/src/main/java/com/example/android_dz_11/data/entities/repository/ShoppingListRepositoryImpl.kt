5d75f7e0ad5e95d4517878b8290d9bbf6b170d6d
Tat-T <ring_87_87@mail.ru>
2025-06-18 20:17:56 +0300

    18.06.25

---
+++ app/src/main/java/com/example/android_dz_11/data/entities/repository/ShoppingListRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.android_dz_11.data.entities.repository
+
+class ShoppingListrepositoryImpl {
+}
---
+++ app/src/main/java/com/example/android_dz_11/domain/model/ShoppingList.kt
@@ -0,0 +1,3 @@
+package com.example.android_dz_11.domain.model
+
+data class ShoppingList()
